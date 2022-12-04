942af4b2ee3a49bcfac6f7bb4c0ac895cff10b8a
Tuladhar <safala.tuladhar@cotiviti.com>
2022-12-04 17:33:58 +0545

    Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardsComponent } from './components/cards/cards.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { PlaygroundComponent } from './playground/playground.component';
+// import { SpecialDirective } from './directives/special.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    // SpecialDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent {
+export class PlaygroundComponent  implements OnInit{
+Username:String='';
+  ngOnInit(): void {
+  }
+
+  onReset(){
+    this.Username = "";
+  }
+  
 
 }
